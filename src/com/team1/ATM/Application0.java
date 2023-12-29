e41aab6bbfab1b1d4297a165688901917c41dfce
jeehwan98 <jeehwan98@naver.com>
2023-12-29 18:04:08 +0900

    stage1 1

---
+++ src/com/team1/ATM/Application0.java
@@ -0,0 +1,9 @@
+package com.team1.ATM;
+
+public class Application0 {
+
+    public static void main(String[] args) {
+
+        System.out.println("testMethod");
+    }
+}
--- src/com/team1/ATM/test.java
+++ src/com/team1/ATM/test.java
@@ -5,5 +5,7 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("git bash를 써보자~");
+
+        System.out.println("hello~!");
     }
 }
